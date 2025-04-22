34d3ae843debdb79a335b4e7215210e4c69f6267
DenWaritthon <waritt10@gmail.com>
2025-04-22 08:48:27 +0700

    ReStructure#4 - add Code to pkg

---
+++ limo_controller/scripts/path_tracking_stanley_control.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float64
+import math
+import numpy as np
+import yaml
+import os
+from ament_index_python.packages import get_package_share_directory
+from tf_transformations import euler_from_quaternion
+
+
+class PathTrackingStanleyController(Node):
+    def __init__(self):
+        super().__init__('path_tracking_stanley_controller')
+                # Load the path from the path.yaml file
+        path_file = os.path.join(get_package_share_directory('lab1_mobile_ackermann'),'config','path.yaml')
+        
+        with open(path_file, 'r') as file:
+            self.path = yaml.safe_load(file)
+
+        # Subscriber
+        self.odom_subscriber = self.create_subscription(Odometry, '/ground_truth/pose', self.odom_callback, 10)
+
+        # Publisher
+        self.cmd_vel_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+
+        # Timer
+        self.timer = self.create_timer(0.1, self.control_loop)
+
+        # Variable
+        self.wheelbase = 0.2
+        self.track = 0.13
+        self.wheel_radius = 0.045
+
+        self.target_x = 0.0
+        self.target_y = 0.0
+        self.target_yaw = 0.0
+        self.path_index = 0
+
+        self.kp_v = 1.0
+        self.k_cross = 1.0 # Gain for cross-track error
+        self.k_soft = 0.1  # Small constant to avoid instability
+
+        self.last_error = 0.0
+        self.yaw_error = 0.0
+
+        self.update_target()
+        self.get_logger().info(f'Node PathTrackingStanleyController Start!!!!!')
+             
+    def odom_callback(self, msg:Odometry):
+        self.current_x = msg.pose.pose.position.x
+        self.current_y = msg.pose.pose.position.y
+        orientation_q = msg.pose.pose.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        roll, pitch, yaw = euler_from_quaternion(orientation_list)
+        self.current_yaw = yaw
+    
+    def update_target(self):
+        self.target_x = self.path[self.path_index]['x']
+        self.target_y = self.path[self.path_index]['y']
+        self.target_yaw = self.path[self.path_index]['yaw']
+
+    def publish_cmd(self, linear, angular):
+        """Publishes the velocity command."""
+        twist_msg = Twist()
+        twist_msg.linear.x = linear
+        twist_msg.angular.z = angular
+        self.cmd_vel_publisher.publish(twist_msg)
+
+    def control_loop(self):           
+        if self.last_error < 1:
+            if self.path_index+1 < len(self.path):
+                self.path_index += 1
+                self.update_target()
+            else:
+                self.publish_cmd(0.0, 0.0)
+                self.get_logger().info('Path tracking completed lap')
+                return
+            
+        # Calculate the error
+        error_x = self.target_x - self.current_x
+        error_y = self.target_y - self.current_y
+        error_yaw = math.atan2(error_y, error_x) - self.current_yaw
+        error_yaw = math.atan2(math.sin(error_yaw), math.cos(error_yaw))
+        
+        distance_error = math.sqrt(error_x**2 + error_y**2)
+        
+        # Stanley Controller
+        control_linear = 0.5
+
+        # Compute cross-track error
+        front_axle_error = np.dot(
+            np.array([np.cos(self.current_yaw + np.pi / 2), np.sin(self.current_yaw + np.pi / 2)]),
+            np.array([error_x, error_y])
+        )
+
+         # Stanley Control Law
+        cross_track_steering = np.arctan2(self.k_cross * front_axle_error, control_linear + self.k_soft)
+        control_angular = error_yaw + cross_track_steering
+               
+        # Limit the speed
+        control_linear = np.clip(control_linear, -0.5, 0.5)
+        control_angular = np.clip(control_angular, -1, 1)
+
+        # Publish Control Commands
+        self.publish_cmd(control_linear, control_angular)
+
+        self.get_logger().info(f'Index: {self.path_index} Target: {self.target_x}, {self.target_y}, {self.target_yaw}')
+        self.get_logger().info(f'Error: {distance_error}, Yaw Error: {error_yaw}, Linear: {control_linear}, Angular: {control_angular}') 
+
+        # Update the error
+        self.last_error = distance_error
+        self.yaw_error = error_yaw      
+        
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PathTrackingStanleyController()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ limo_model/launch/limo_sim.launch.py
@@ -0,0 +1,150 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch_ros.substitutions import FindPackageShare
+
+import launch_ros.actions
+import xacro
+
+def generate_launch_description():
+    package_name = "lab1_mobile_ackermann"
+
+    # Spawn the robot in the world
+    spawn_x_val = '9.0'
+    spawn_y_val = '0.0'
+    spawn_z_val = '0.0'
+    spawn_yaw_val = '1.57'
+
+    rsp = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [
+                    os.path.join(
+                        get_package_share_directory(package_name),
+                        "launch",
+                        "limo_description.launch.py"
+                    )
+                ]
+            ),
+            launch_arguments={"use_sim_time":"true"}.items()
+        )
+    
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name),
+                    "launch",
+                    "world.launch.py"
+                )
+            ]
+        )
+    )
+
+    spawn_entity = Node(
+        package='gazebo_ros', 
+        executable='spawn_entity.py',
+        arguments=['-entity', 'limo', 
+                '-topic', 'robot_description',
+                '-timeout', '120.0',
+                    '-x', spawn_x_val,
+                    '-y', spawn_y_val,
+                    '-z', spawn_z_val,
+                    '-Y', spawn_yaw_val],
+        output='screen'
+    )
+
+    rviz_file_path = os.path.join(
+        get_package_share_directory(package_name),
+        'rviz',
+        'display.rviz'
+    )
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=[
+            "-d", rviz_file_path
+        ],
+        output = "screen"
+    )
+
+    joint_state_broadcaster_spawner = Node(
+            package="controller_manager",
+            executable="spawner",
+            arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+            parameters=[{"use_sim_time": True}]
+        )
+
+    velocity_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["velocity_controllers", "--controller-manager", "/controller_manager"],
+        parameters=[{"use_sim_time": True}]
+    )
+
+    steering_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["steering_controller", "--controller-manager", "/controller_manager"],
+        parameters=[{"use_sim_time": True}]
+    )
+    
+
+    launch_description = LaunchDescription()
+
+    launch_description.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[joint_state_broadcaster_spawner],
+            )
+        )
+    )
+
+    launch_description.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[velocity_controller_spawner],
+            )
+        )
+    )
+
+    launch_description.add_action(
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=velocity_controller_spawner,
+                on_exit=[steering_controller_spawner],
+            )
+        )
+    )
+    
+    # Static Transform Publisher (world -> odom)
+    static_tf_world_odom = launch_ros.actions.Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=[spawn_x_val, spawn_y_val, spawn_z_val, spawn_yaw_val, "0", "0", "world", "odom"],
+        output="screen"
+    )
+
+    # Static Transform Publisher (world -> base_footprint)
+    static_tf_world_base = launch_ros.actions.Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments=[spawn_x_val, spawn_y_val, spawn_z_val, spawn_yaw_val, "0", "0", "world", "base_footprint"],
+        output="screen"
+    )
+
+    launch_description.add_action(gazebo)
+    launch_description.add_action(rsp)
+    launch_description.add_action(spawn_entity)
+    launch_description.add_action(rviz)
+    launch_description.add_action(static_tf_world_odom)
+    launch_description.add_action(static_tf_world_base)
+
+    return launch_description
